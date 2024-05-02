ebd2eec2768672a0a40dd6e959d2d3e5bea6c875
Mahmoud Abdullah Anani <99493135+MahmoudAbdullahAnani@users.noreply.github.com>
2024-05-02 18:42:56 +0300

    add icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import CheckIsAdmin from "@/components/CheckIsAdmin";
 import ControlPanel from "@/components/Dashboard/ControlPanel";
 
+
 export const metadata: Metadata = {
   title: "Ajwaa Dashboard",
   description: "This is Ajwaa for Dashboard Template",
