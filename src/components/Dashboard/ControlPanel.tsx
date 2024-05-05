608a4eae4c1462d12563b669d9259a5f8644132f
Mahmoud Abdullah Anani <99493135+MahmoudAbdullahAnani@users.noreply.github.com>
2024-05-05 20:45:24 +0300

    check error

--- src/components/Dashboard/ControlPanel.tsx
+++ src/components/Dashboard/ControlPanel.tsx
@@ -221,14 +221,14 @@ const ControlPanel: React.FC = () => {
       </div>
 
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
-        <CharTrip cashData={cashData} />
-        {/* <MainChartsTop
+        {/* <CharTrip cashData={cashData} /> */}
+        <MainChartsTop
           allUsers={allUsers}
           allUsersActive={allUsersActive}
           allUsersUnActive={allUsersUnActive}
           // @ts-ignore
           cashData={cashData}
-        ></MainChartsTop> */}
+        ></MainChartsTop>
         <ChartOne />
         <ChartTwo />
         <ChartThree />
